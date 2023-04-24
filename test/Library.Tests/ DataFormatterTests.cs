a9976641bef94948734595f11fb3102c6b8245b5
fedepds <federico.pizarro@correo.ucu.edu.uy>
2023-04-24 20:58:44 -0300

    Cree los test

---
+++ test/Library.Tests/ DataFormatterTests.cs
@@ -0,0 +1,40 @@
+
+using TestDateFormat;
+using NUnit.Framework;
+
+
+namespace LibraryTest;
+
+public class DateFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestRightDateFormat()
+    {
+        const string input = "17/11/2023";
+        const string expected = "2023-11-17";
+        Assert.That(DateFormatter.ChangeFormat(input), Is.EqualTo(expected));
+        
+    }
+    [Test]
+    public void TestWrongDateFormat()
+    {
+        const string input = "17/011/2023";
+        const string expected = "";
+        Assert.That(DateFormatter.ChangeFormat(input), Is.EqualTo(expected));
+    }
+    [Test]
+ /// </summary>   
+
+    public void TestWhiteDate()
+    {
+        const string input = "";
+        const string expected = "";
+        Assert.That(DateFormatter.ChangeFormat(input), Is.EqualTo(expected));
+    }
+
+}
